9cc921ed29bc347445e6d9d5eb2d6d013a543eb3
Carlos Aguilar <carlosAguilar@Carloss-MacBook-Pro.local>
2017-05-05 17:03:11 +0200

    Adding mongoDB interaction using pyMongo

---
+++ handleMongoDB.py
@@ -0,0 +1,31 @@
+# import the usuals
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import pandas_datareader.data as web
+import numpy as np
+from pymongo import MongoClient
+from readStocks import read_stocks, normaliseStocks
+import pprint
+
+client = MongoClient('mongodb://localhost:27017/')
+
+# create a collection named 'yahooStocks'
+db = client['yahooStocks']
+stocksCollection = db['stocksCollection']
+
+# read the collection of stocks
+listOfTickers = [['AAPL', 'MSFT']]
+stocks = read_stocks(listOfSymbols = listOfTickers);
+# insert into mongoDB - use the option 'records' to keep the columnnames
+result = stocksCollection.insert_many(stocks.to_dict('records'));
+#result = stocksCollection.insert_many(stocks.to_dict('index'));
+# check the process went well
+result.inserted_ids
+
+pprint.pprint(stocksCollection.find_one())
+
+# retrieve as a DF
+df = pd.DataFrame(list(stocksCollection.find()))
+df.describe()
+# stocksCollection.remove() to drop them all
